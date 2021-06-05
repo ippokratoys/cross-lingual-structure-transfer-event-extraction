b0a3a482cb32af598a6c94eba89325223cac01e0
Thanasis <thanasisp@theblueground.com>
2021-06-05 11:48:25 +0300

    Add data transformation script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cross-lingual-structure-transfer-event-extraction
 Implementation of Cross-lingual Structure Transfer for Relation and Event Extraction
+
+## Data pre-processing
+
+In order to pre-process the data use the script given in `data-preprocessing/`.
+
+### Convert Rams data
+In order to convert RAMS data use transform_data.py. It takes two arguments `--source` 
+for defining the source file (see `data/rams`) and `--target` to define the output file
+
+e.g `python data-preprocessing/transform_data.py --source data/rams/dev.jsonlines --target data/parsed/dev.json`
---
+++ data-preprocessing/transform_data.py
@@ -0,0 +1,79 @@
+"""
+Transform the RAMS data to a json that will be used
+in order to apply event extraction. The target JSON
+file will have the following format
+[{
+  sentence: "I will travel to Greece next week."
+  event: TRANSFER
+}]
+"""
+import argparse
+import json
+from pathlib import Path
+
+
+def get_event(events, sentence_start_index, sentence_end_index):
+    """
+    Returns whether a sentence given its start/end index contains an event.
+    If a event exists it returns the event as a string else NONE
+    """
+    for event in events:
+        event_start = event[0]
+        event_end = event[1]
+        event = event[2][0][0]
+        if sentence_start_index <= event_start <= sentence_end_index:
+            return event
+        if sentence_start_index <= event_end <= sentence_end_index:
+            return event
+
+    return None
+
+
+def convert_line_to_target(line):
+    """
+    Given a line of .jsonline parse the line and convert it to a list of sentences
+    with their event
+    :param line: a line from RAMS dataset
+    :return: a list of sentence with events
+    """
+    data = json.loads(line)
+    events = data['evt_triggers']
+    sentences = data['sentences']
+    sentence_start_index = 0
+    sentences_converted = []
+    for sentence_tokes in sentences:
+        sentence_end_index = sentence_start_index + len(sentence_tokes)
+
+        entry = dict()
+        entry['sentence'] = " ".join(sentence_tokes)
+
+        sentence_event = get_event(events, sentence_start_index, sentence_end_index)
+        if sentence_event is None:
+            entry['event'] = 'NONE'
+        else:
+            entry['event'] = sentence_event
+
+        sentence_start_index = sentence_end_index
+        sentences_converted.append(entry)
+
+    return sentences_converted
+
+
+def transform_from_file_to_file(source_file_path, target_file_path):
+    with open(source_file_path, "r") as read_file:
+        sentences = []
+        for line in read_file:
+            sentences.extend(convert_line_to_target(line))
+
+        path = Path(target_file_path)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with open(target_file_path, "w") as write_file:
+            json.dump(sentences, write_file, indent=2)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', default="data/rams/dev.jsonlines")
+    parser.add_argument('--target', default="data/parsed/dev.json")
+    args = parser.parse_args()
+    transform_from_file_to_file(args.source, args.target)
