47e51688d9dfd1d778d540ed67931057e8d0fff3
Thanasis <thanasisp@theblueground.com>
2021-06-08 20:11:12 +0300

    Add udpipe related data to dev.json

--- data-preprocessing/transform_data.py
+++ data-preprocessing/transform_data.py
@@ -11,6 +11,35 @@ import argparse
 import json
 from pathlib import Path
 
+from corpy.udpipe import Model
+
+m = Model("./data/english-ewt-ud-2.5-191206.udpipe")
+
+
+def map_word_to_token(word):
+    """
+    Given a sentence return a list of tokens with the required data
+
+    :param sentence: a sentence
+    :return: a list of sentence with events
+    """
+    token = {}
+    token['text'] = word.form
+    token['part_of_speech'] = word.upostag
+    token['id'] = word.id
+    token['dependency_relation'] = word.deprel
+    token['head'] = word.head
+    return token
+
+
+def map_tokens(sentence):
+    """
+    :param sentence: a sentence as string
+    :return: a list of tokens with the required data
+    """
+    parsed = list(m.process(sentence))
+    return list(map(map_word_to_token, parsed[0].words[1:]))
+
 
 def get_event(events, sentence_start_index, sentence_end_index):
     """
@@ -46,6 +75,7 @@ def convert_line_to_target(line):
 
         entry = dict()
         entry['sentence'] = " ".join(sentence_tokes)
+        entry['tokens'] = map_tokens(entry['sentence'])
 
         sentence_event = get_event(events, sentence_start_index, sentence_end_index)
         if sentence_event is None:
@@ -68,7 +98,7 @@ def transform_from_file_to_file(source_file_path, target_file_path):
         path = Path(target_file_path)
         path.parent.mkdir(parents=True, exist_ok=True)
         with open(target_file_path, "w") as write_file:
-            json.dump(sentences, write_file, indent=2)
+            json.dump(sentences, write_file, separators=(',', ':'))
 
 
 if __name__ == '__main__':
